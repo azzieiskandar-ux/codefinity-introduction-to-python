1661e59b2e3a4a5f87e6413fe7ee5e2c9ca2e1ba
azzieiskandar-ux <>
2026-02-09 13:38:03 +0000

    Type Comparisons

---
+++ conditional_statements/type_comparisons/main.py
@@ -0,0 +1,10 @@
+# Product details
+description = "Imported honey, raw and unfiltered"
+price = "5.99"
+count = 120
+
+# Write your code here
+
+
+print("Is price a float?:", price_is_float)
+print("Is count an integer?:", count_is_int)
